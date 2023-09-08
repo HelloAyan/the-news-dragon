1f6c2ce680e0b7009c8d609d2fef1d502720da7d
helloayan <18103373@iubat.edu>
2023-09-08 19:29:01 +0600

    Date & Marquee text added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import logo from '../../../assets/logo.png';
+import moment from 'moment';
+import { Container, Button } from 'react-bootstrap';
+import Marquee from 'react-fast-marquee';
 
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="Logo Image" />
-        </div>
+        <Container>
+            <div className="text-center">
+                <img src={logo} alt="Logo Image" />
+                <p className='text-secondary'><small>Journalism Without Fear or Favor</small></p>
+                <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+            </div>
+
+            <div className='d-flex bg-light p-2'>
+                <Button variant="danger">Danger</Button>
+                <Marquee className='text-danger' speed={70}>
+                    <span> <strong>Match Highlights :  </strong> </span> &nbsp; Germany  vs Spain — as it happened   !   Match Highlights: Germany vs Spain as... Match Highlights: Germany vs Spain as...
+                </Marquee>
+            </div>
+        </Container>
     )
 }
 
