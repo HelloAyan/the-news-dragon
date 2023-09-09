44e209e5d657491ae528cb40198d5ceff5b54b87
helloayan <18103373@iubat.edu>
2023-09-09 15:19:48 +0600

    Show data from categories api

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const LeftNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then((res) => res.json())
+            .then((data) => setCategories(data))
+            .catch((error) => { console.log(error) })
+    }, [])
+
+    console.log(categories);
     return (
-        <div><h2>Left Nav</h2></div>
+        <div>
+            <h5>All Category</h5>
+
+            <div className='ps-4'>
+                {categories.map(category =>
+                    <p key={category.id}>
+                        <Link to={`/category/${category.id}`} className='text-decoration-none text-black'>{category.name}</Link>
+                    </p>
+                )}
+
+            </div>
+
+        </div>
+
     )
 }
 
