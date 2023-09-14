c95e066a57a827fd6eb50b22ba28eadd4cc429fc
helloayan <18103373@iubat.edu>
2023-09-15 01:14:47 +0600

    News Card Header and body data displayed

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,12 +1,24 @@
+import moment from 'moment';
 import React from 'react'
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Image } from 'react-bootstrap';
+import { FaEye, FaRegBookmark, FaShareAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const NewsCard = ({ news }) => {
-    const { _id, title, details, image_url } = news;
+    const { _id, title, details, image_url, author, total_view, rating } = news;
     return (
         <Card className="mb-4">
-            <Card.Header>Featured</Card.Header>
+            <Card.Header className='d-flex align-items-center'>
+                <Image style={{ height: '35px' }} src={author?.img} roundedCircle alt='Author Image' title='Author Img' />
+                <div className='ps-2 flex-grow-1' >
+                    <p className='m-0 p-0'>{author?.name}</p>
+                    <p className='m-0 p-0'><small>  {moment(author.published_date).format("YYYY-MM-D")} </small> </p>
+                </div>
+                <div>
+                    <FaRegBookmark  ></FaRegBookmark>
+                    <FaShareAlt className='ms-1'></FaShareAlt>
+                </div>
+            </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Img variant="top" src={image_url} />
@@ -15,7 +27,13 @@ const NewsCard = ({ news }) => {
                 </Card.Text>
 
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="text-muted d-flex">
+                <div className='flex-grow-1'> <span>{rating?.number}</span> </div>
+                <div>
+                    <FaEye></FaEye> {total_view}
+                </div>
+
+            </Card.Footer>
         </Card>
     )
 }
