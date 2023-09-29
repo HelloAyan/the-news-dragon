7f406dbc273bb590f45660b8a42bf23334866d9b
helloayan <18103373@iubat.edu>
2023-09-29 19:44:27 +0600

    Login component design completed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <Container className='w-50 mt-4'>
+            <h3 className='text-center mb-4'>Please Login</h3>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+
+                <Form.Group className='mt-2'>
+                    <Form.Text className="text-success">
+                        Don't have an account? <Link to={'/register'}>Register Now!</Link>
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Text className="text-success">
+
+                </Form.Text>
+                <Form.Text className="text-danger">
+
+                </Form.Text>
+            </Form>
+        </Container>
     )
 }
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import {
     createBrowserRouter,
+    Navigate,
     RouterProvider,
     useLoaderData,
 } from "react-router-dom";
@@ -8,15 +9,37 @@ import Home from "../pages/Home/Home/Home";
 import Category from "../pages/Home/Category/Category";
 import NewsLayout from "../layouts/NewsLayout";
 import News from "../pages/News/News/News";
+import LoginLayout from "../layouts/LoginLayout";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <LoginLayout></LoginLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to={'/category/0'}></Navigate>
+            },
+
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: 'register',
+                element: <Register></Register>
+            }
+        ]
+    },
     {
         path: "category",
         element: <Main></Main>,
         children: [
 
             {
-                path: '/:id',
+                path: ':id',
                 element: <Category></Category>,
                 loader: ({ params }) => fetch(`http://localhost:5000/categories/${params.id}`),
             }
