b1c92dfcaa0936e48f3d64803d9bf217c674c0c8
HelloAyan <18103373@iubat.edu>
2023-11-03 17:32:19 +0600

    modified login with google authentication

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,6 +1,6 @@
 
-import React from 'react'
-import { Button, ListGroup } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Button, ListGroup, Spinner } from 'react-bootstrap';
 import { FaGoogle, FaGithub, FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 import Qzone from '../Qzone/Qzone';
 import bg from '../../../assets/bg.png'
@@ -8,15 +8,40 @@ import { useContext } from 'react';
 import { AuthContext } from '../../../providers/AuthProvider';
 
 
-
 const RightNav = () => {
     const { googleSignIn, githubSignIn } = useContext(AuthContext);
+    const [loading, setLoading] = useState(false);
+
+    const handleGoogleSignIn = async () => {
+        setLoading(true); // Show loading indicator
+        await googleSignIn();
+        setLoading(false); // Hide loading indicator when the response is received
+    };
+
+    const handleGithubSignIn = async () => {
+        setLoading(true); // Show loading indicator
+        await githubSignIn();
+        setLoading(false); // Hide loading indicator when the response is received
+    };
+
     return (
         <div>
             <div>
                 <h5>Login With</h5>
-                <Button variant="outline-primary" className='mb-2' onClick={googleSignIn} > <FaGoogle /> Login with Google</Button>
-                <Button variant="outline-secondary" onClick={githubSignIn}> <FaGithub /> Login with Github</Button>
+                <Button variant="outline-primary" className='mb-2' onClick={handleGoogleSignIn} >
+                    {loading ? (
+                        <Spinner animation="border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                    ) : (
+                        <> <FaGoogle /> <span>Login with Google</span> </>
+                    )} </Button>
+                <Button variant="outline-secondary" onClick={handleGithubSignIn}> <FaGithub /> Login with Github</Button>
+                {loading && (
+                    <Spinner animation="border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+                )}
             </div>
 
             <div className='mt-4'>
