db4cdf2d91c8f57d75cbd44360122d59e7649825
HelloAyan <18103373@iubat.edu>
2023-11-11 18:59:26 +0600

    modified

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import useTitle from '../../hook/useTitle';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Contact = () => {
     useTitle('Contact')
     return (
-        <h3 className='text-center pt-5 '>Contact Coming Soon...</h3>
+        <div>
+            <h3 className='text-center pt-5 '>Contact Coming Soon...</h3>
+            <Container>
+                <Row>
+                    <Col lg={6}>
+                        <div>Get in Touch</div>
+                    </Col>
+                    <Col lg={6}>
+                        map
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+
     )
 }
 
